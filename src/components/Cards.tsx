07c2039709c69da1e9b301e2f9667b4077abaa61
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2023-01-30 16:46:25 +0530

    fixed session storage bug about not displaying the content

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,11 +1,10 @@
-import { addToCart } from '../features/counter/counterSlice';
-import { useAppDispatch } from '../hooks'
-import Cardsdata from '../Data/Data'
-import YourCart from './YourCart';
+import { addToCart } from "../features/counter/counterSlice";
+import { useAppDispatch } from "../hooks";
+import Cardsdata from "../Data/Data";
+import YourCart from "./YourCart";
 function Cards() {
-    
-    const dispatch = useAppDispatch();
-    
+  const dispatch = useAppDispatch();
+
   return (
     <div className="flex mr-20 ml-20 mt-10 justify-around">
       <div className="grid grid-cols-3 gap-10">
@@ -13,10 +12,10 @@ function Cards() {
           return (
             <div key={item.id}>
               <h2 className="text-3xl">{item.rname}</h2>
-              <img className="w-[300px]" src={item.imgdata} alt="img"/>
+              <img className="w-[300px]" src={item.imgdata} alt="img" />
               <button
                 onClick={() => {
-                  dispatch(addToCart(item))
+                  dispatch(addToCart(item));
                 }}
                 className="h-[60px] bg-slate-300 rounded-xl w-full hover:bg-slate-400"
               >
@@ -28,7 +27,7 @@ function Cards() {
       </div>
       <YourCart />
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
--- src/components/YourCart.tsx
+++ src/components/YourCart.tsx
@@ -1,34 +1,44 @@
 import { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
-import { addToCart, deleteItem, decrement } from "../features/counter/counterSlice";
+import {
+  addToCart,
+  deleteItem,
+  decrement,
+} from "../features/counter/counterSlice";
 
 function YourCart() {
-  let data = useAppSelector((state) => state.counter.cart)
-    const [price, setPrice] = useState(0)
-    
-    const dispatch = useAppDispatch();
+  let data = useAppSelector((state) => state.counter.cart);
+  const [price, setPrice] = useState(0);
 
-    useEffect(() => {
-      let sum = 0;
-      data.map(item => {
-          return(
-              sum = sum + item.qnty * item.price
-          )
-        })
-        setPrice(sum)
-        sessionStorage.setItem("key", JSON.stringify(data))
-    }, [data])
+  const dispatch = useAppDispatch();
 
-    const sessionData = JSON.parse(sessionStorage.getItem("key") || '{}')
-    
-    if(data.length === 0){
-      data = sessionData
-    }
-    
+  useEffect(() => {
+    let sum = 0;
+    data.map((item) => {
+      return (sum = sum + item.qnty * item.price);
+    });
+    setPrice(sum);
+  }, [data]);
+
+  useEffect(() => {
+    sessionStorage.setItem("key", JSON.stringify(data));
+  });
+
+  let storage = JSON.parse(sessionStorage.getItem("key") || "{}") as {
+    id: number;
+    rname: string;
+    price: number;
+    qnty: number;
+  }[];
+
+  if (Object.keys(storage).length === 0) {
+    storage = data;
+  }
+  console.log(Object.keys(storage).length === 0);
+
   return (
     <div>
-      <h2>heading</h2>
-      {data.map((item) => {
+      {storage.map((item) => {
         return (
           <div className="m-2" key={item.id}>
             <div className="flex text-2xl">
@@ -37,16 +47,31 @@ function YourCart() {
                 <p>₹ {item.price}</p>
                 <p>Quantity: {item.qnty}</p>
               </div>
-              <button className="bg-slate-500 text-white px-4 mx-4 my-6 rounded-xl" onClick={() => dispatch(decrement(item))}>-</button>
-              <button className="bg-slate-500 text-white px-4 mx-4 my-6 rounded-xl" onClick={() => dispatch(addToCart(item))}>+</button>
-              <button onClick={() => dispatch(deleteItem(item.id))} className="bg-slate-400 text-white p-1 my-5 rounded ml-5">Delete</button>
+              <button
+                className="bg-slate-500 text-white px-4 mx-4 my-6 rounded-xl"
+                onClick={() => dispatch(decrement(item))}
+              >
+                -
+              </button>
+              <button
+                className="bg-slate-500 text-white px-4 mx-4 my-6 rounded-xl"
+                onClick={() => dispatch(addToCart(item))}
+              >
+                +
+              </button>
+              <button
+                onClick={() => dispatch(deleteItem(item.id))}
+                className="bg-slate-400 text-white p-1 my-5 rounded ml-5"
+              >
+                Delete
+              </button>
             </div>
           </div>
         );
       })}
       <h2 className="text-3xl">Total: ₹{price}</h2>
     </div>
-  )
+  );
 }
 
-export default YourCart
+export default YourCart;
